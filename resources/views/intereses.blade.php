f1ee7ba5d831b23b085f8217665a2f1e4ab457d6
Juan Camilo Gomez <jcami9893@gmail.com>
2026-02-18 20:16:35 -0500

    Vista intereses.blade.php creada

---
+++ resources/views/intereses.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sección Intereses</title>
+</head>
+<body>
+    <h1>🎯 INTERESES</h1>
+    <ul>
+    <li>Jugar Videojuegos</li>
+    <li>Escuchar Música</li>
+    <li>Parchar</li>
+    </ul>
+</body>
+</html>
--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sección Perfil</title>
+</head>
 <body>
-    <div class="perfil-card">
-        <h1>👤 PERFIL</h1>
-        <div class="perfil-contenido">
-            <h2>Nombre: Juan Camilo Gómez Roa</h2>
-            <h3>Edad: 18</h3>
-            <h3>Universidad: Unab</h3>
-            <h3>Semestre: Quinto</h3>
-        </div>
-    </div>
+    <h1>👤 PERFIL</h1>
+    <h2>Nombre: Juan Camilo Gómez Roa</h2>
+    <h3>Edad: 18</h3>
+    <h3>Universidad: Unab</h3>
+    <h3>Semestre: Quinto</h3>
 </body>
 </html>
