6c40cc9e7a4998a42e6ba0bdf59fe55cf2e5c22e
Juan Camilo Gomez <jcami9893@gmail.com>
2026-02-18 20:54:38 -0500

    Hoja de estilos CSS implementada

--- resources/views/intereses.blade.php
+++ resources/views/intereses.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sección Intereses</title>
+    <link rel="stylesheet" href="{{asset('css/estilos.css')}}">
 </head>
 <body>
     <h1>🎯 INTERESES</h1>
