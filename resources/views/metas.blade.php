d6d6df7df6275d3eebaed62498d412faa1030b50
Juan Camilo Gomez <jcami9893@gmail.com>
2026-02-18 20:35:15 -0500

    Vista metas.blade.php creada

---
+++ resources/views/metas.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sección Metas</title>
+</head>
+<body>
+    <h1>🚀 METAS</h1>
+    <h3>Corto plazo:</h3>
+    <ul><li>Terminar el taller</li></ul>
+    <ul><li>Terminar la carrera</li></ul>
+    <ul><li>Comprender Laravel</li></ul>
+    <h3>Largo plazo:</h3>
+    <ul><li>Ejercer mi profesión</li></ul>
+    <ul><li>Salir del país</li></ul>
+    <ul><li>Terminar el taller</li></ul>
+</body>
+</html>
