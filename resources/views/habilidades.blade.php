530e5dbe908e5a7108c6fea96487962ec52fbb72
Juan Camilo Gomez <jcami9893@gmail.com>
2026-02-18 20:29:26 -0500

    Vista habilidades.blade.php creada

---
+++ resources/views/habilidades.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sección Habilidades</title>
+</head>
+<body>
+    <h1>⚡ HABILIDADES</h1>
+    <h3>Técnicas:</h3>
+    <ul>
+    <p>HTML</p>
+    </ul>
+    <ul>
+    <p>CSS</p>
+    </ul>
+    <ul>
+    <p>Python</p>
+    </ul>
+    <ul>
+    <p>JavaScript</p>
+    </ul>
+    <h3>Blandas:</h3>
+    <ul>
+    <p>Trabajo en equipo</p>
+    </ul>
+    <ul>
+    <p>Liderazgo</p>
+    </ul>
+    <ul>
+    <p>Dedicación</p>
+    </ul>
+</body>
+</html>
