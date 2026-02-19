2f0212d1e9c66ddc82440d8f9fcd28ccdebb7aa3
Juan Camilo Gomez <jcami9893@gmail.com>
2026-02-18 20:02:03 -0500

    Vista perfil.bade.php creada

---
+++ resources/views/perfil.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<body>
+    <div class="perfil-card">
+        <h1>👤 PERFIL</h1>
+        <div class="perfil-contenido">
+            <h2>Nombre: Juan Camilo Gómez Roa</h2>
+            <h3>Edad: 18</h3>
+            <h3>Universidad: Unab</h3>
+            <h3>Semestre: Quinto</h3>
+        </div>
+    </div>
+</body>
+</html>
